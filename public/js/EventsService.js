8464dd7f3075f5e8075d363be705e9a4f57c8f4a
sourabhs <suman.s@husky.neu.edu>
2015-11-14 18:56:34 -0500

    keyword search from menu bar

--- public/js/EventsService.js
+++ public/js/EventsService.js
@@ -1,17 +1,13 @@
 ﻿app.factory("EventsService", function ProductService($http) {
 
-	var fetchEventsByLocation = function(param_map, callback){
-		console.log(param_map);
+	var fetchEventsByLocation = function(param_map, responseHandler){
 		var query = "";
-		var key ;
 		for(key in param_map){
 			query += "&" + key + "=" + param_map[key];
 		}
 		
-//		var latitude = "location.latitude=" + location_latitude;
-//		var longitude = "location.longitude=" + location_longitude;
 		$http.get("https://www.eventbriteapi.com/v3/events/search/?token=SVLBJRZ4G7ATSPI77JQ3&format=json" + query)
-		.success(callback);
+		.success(responseHandler);
 	}
 
 	return {
