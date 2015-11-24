e427b7ea7692ef50781f135c178973c6fae35e2d
visha <visha@ccs.neu.edu>
2015-11-24 12:58:30 -0500

    Fixed null user login error

--- public/controllers/LoginController.js
+++ public/controllers/LoginController.js
@@ -1,18 +1,19 @@
 app.controller('LoginController', function($scope, $rootScope, $location, DbService){
 	$scope.login = function(user) {
-		
 		$scope.loginError = false;
 		var userCredentials = new UserCredential();
-		userCredentials.setCredentials(user.username, user.password);
-		DbService.login(user, function(response){
-			response = getObjectIfAvailable(response);
-			if(response){
-				$rootScope.currentUser = userCredentials.username;
-				$location.url('/home');
-			}
-			else {
-				$location.url('/login');
-			}	
-		})
+		if (getStringObjectIfAvailable(user) && getStringObjectIfAvailable(user.username) && getStringObjectIfAvailable(user.password)) {
+			userCredentials.setCredentials(user.username, user.password);
+			DbService.login(user, function(response){
+				response = getObjectIfAvailable(response);
+				if(response){
+					$rootScope.currentUser = userCredentials.username;
+					$location.url('/home');
+				}
+				else {
+					$location.url('/login');
+				}	
+			})
+		}
 	}
 })
