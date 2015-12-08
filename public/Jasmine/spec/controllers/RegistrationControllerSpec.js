6b620246b8817d34442b208f6f0935c0125e51e9
visha <visha@ccs.neu.edu>
2015-12-07 21:26:09 -0500

    Added tests for UserProfileUpdate controller

--- public/Jasmine/spec/controllers/RegistrationControllerSpec.js
+++ public/Jasmine/spec/controllers/RegistrationControllerSpec.js
@@ -16,43 +16,29 @@ describe("Unit tests for RegistrationController", function (service) {
 		});
 	}));
 
-	describe('Unit tests for registration handling successful insertion into registration and login tables', function () {
+	describe('Unit tests for registration handling insertions into registration and login tables', function () {
 		var resp = window.getJSONFixture('../../../../responses/userResponse.json');
-		beforeEach(function(){						
+
+		it("tests if successful insertion into registration and login tables is handles", function () {
 			$httpBackend.whenPOST('/user/addNewDetails').respond(200, resp);
 			$httpBackend.whenPOST('/user/addNewLogin').respond(200, resp);
 			scope.register(user);
 			$httpBackend.flush();
-		});
-
-		it("test if invalid response is handled", function () {
 			expect(scope.registrationStatus).toEqual('Registration was successful!!');
 		});
-	});
 
-	describe('Unit tests for registration handling error from login table', function () {
-		var resp = window.getJSONFixture('../../../../responses/userResponse.json');
-		beforeEach(function(){						
+		it("tests if error from login table is handled", function () {
 			$httpBackend.whenPOST('/user/addNewDetails').respond(200,resp);
 			$httpBackend.whenPOST('/user/addNewLogin').respond(1,resp);
 			scope.register(user);
 			$httpBackend.flush();
-		});
-
-		it("test if valid response is handled", function () {
 			expect(scope.registrationStatus).toEqual("Registration failed. Please try again.");			
 		});
-	});
-	
-	describe('Unit tests for registration handling error on registration table', function () {
-		var resp = window.getJSONFixture('../../../../responses/userResponse.json');
-		beforeEach(function(){						
+
+		it("tests if error from registration table is handled", function () {
 			$httpBackend.whenPOST('/user/addNewDetails').respond(1,resp);
 			scope.register(user);
 			$httpBackend.flush();
-		});
-
-		it("test if invalid response is handled", function () {
 			expect(scope.registrationStatus).toEqual("Registration failed. Please try again.");			
 		});
 	});
